2f10313eae6f7a45e7335ecb20d43635848c0422
Werffios <nasuarezro@unal.edu.co>
2023-08-27 11:39:34 -0500

    Routes configured

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return (redirect('/admin'));
 });
+
+Route::get('/login', function () {
+    return (redirect('/admin'));
+});
+
