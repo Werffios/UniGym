a12e82a9f865b9d7b125f81a72a4b770a555339c
Nicolas Suarez <nasuarezro@unal.edu.co>
2023-10-26 22:49:49 -0500

    composer update

--- resources/js/app.js
+++ resources/js/app.js
@@ -1,1 +1,18 @@
 import './bootstrap';
+
+import Alpine from 'alpinejs';
+
+window.Alpine = Alpine;
+
+Alpine.start();
+
+function setSvgFill() {
+    const svg = document.getElementById("my-svg");
+    if (window.matchMedia("(prefers-color-scheme: dark)").matches) {
+        svg.style.fill = "white";
+    } else {
+        svg.style.fill = "black";
+    }
+}
+setSvgFill(); // para establecer el valor de fill al cargar la página
+window.matchMedia("(prefers-color-scheme: dark)").addListener(setSvgFill); // para actualizar el valor de fill cuando cambie el modo
