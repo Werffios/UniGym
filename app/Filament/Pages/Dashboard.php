abd418f8d5eb1c15da79dcc5d4cd1be5c8b5eb04
Nicolas Suarez <nasuarezro@unal.edu.co>
2023-12-13 02:50:41 -0500

    charts

--- app/Filament/Pages/Dashboard.php
+++ app/Filament/Pages/Dashboard.php
@@ -7,10 +7,11 @@ class Dashboard extends \Filament\Pages\Dashboard
     // ...Widgets\AccountWidget::class,
     protected static ?string $title = 'Resúmenes interactivos';
 
+    protected ?string $subheading = 'En esta sección encontrarás los resúmenes interactivos de los diferentes módulos del sistema.';
+
     protected function getHeaderWidgets(): array
     {
         return [
-
         ];
     }
 
