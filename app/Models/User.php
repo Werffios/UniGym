a4c5a23712874582eb0a11bf4438417cdbbcaad4
Nicolas Suarez <nasuarezro@unal.edu.co>
2023-12-12 23:40:41 -0500

    HasRoles

--- app/Models/User.php
+++ app/Models/User.php
@@ -9,11 +9,11 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
-use Spatie\Permission\Models\Role;
+use Spatie\Permission\Traits\HasRoles;
 
 class User extends Authenticatable implements FilamentUser
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles;
 
     public function canAccessPanel(Panel $panel): bool
     {
@@ -30,11 +30,6 @@ class User extends Authenticatable implements FilamentUser
         'email',
         'password',
     ];
-
-    public function roles()
-    {
-        return $this->belongsToMany(Role::class)->withTimestamps();
-    }
 
     /**
      * The attributes that should be hidden for serialization.
