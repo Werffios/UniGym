bf3d989f609a53d312dba14465ac0057036b4281
Nicolas Suarez <nasuarezro@unal.edu.co>
2024-02-10 21:48:24 -0500

    Roles and permissions: bug password required

--- app/Policies/DegreePolicy.php
+++ app/Policies/DegreePolicy.php
@@ -13,7 +13,7 @@ class DegreePolicy
      */
     public function viewAny(User $user): bool
     {
-        return $user->hasPermissionTo('Degree.ViewAny');
+        return $user->hasRole(['Super Admin', 'Administrador']);
     }
 
     /**
